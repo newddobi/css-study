3a8ac071433b33972776137490c311ca5a9ba657
ddobi <qhdqhdekd261@naver.com>
2020-12-31 13:22:14 +0900

    complete merging navigation

--- js/index.js
+++ js/index.js
@@ -1,3 +1,16 @@
-$(document).ready(function(){
-   
+$(document).ready(function () {
+
+    // 모바일 햄버거 메뉴 토글
+    $(".hamburger-checkbox").click(function () {
+        if ($(this).prop("checked") == true) {
+            $(".nav-hamburger").css("display", "block");
+        } else {
+            $(".nav-hamburger").css("display", "");
+        }
+    });
+
+    // 모바일 햄버거 메뉴 중복 방지
+    $(".sub-nav-hamburger-checkbox").click(function (){
+        $(".sub-nav-hamburger-checkbox").not(this).prop('checked', false);  
+    })
 });
