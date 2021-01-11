7214838d618f0965c9b3e6fb3c0cb9b3d3ba81ac
ddobi <qhdqhdekd261@naver.com>
2021-01-11 17:37:49 +0900

    complete homework

--- js/index.js
+++ js/index.js
@@ -12,5 +12,29 @@ $(document).ready(function () {
     // 모바일 햄버거 메뉴 중복 방지
     $(".sub-nav-hamburger-checkbox").click(function (){
         $(".sub-nav-hamburger-checkbox").not(this).prop('checked', false);  
-    })
+    });
+
+    $("#icon_naver_1").click(function(){
+        alert('네이버1')
+    });
+
+    $("#icon_kakao").click(function(){
+        alert('카카오')
+    });
+
+    $("#icon_naver_2").click(function(){
+        alert('네이버2')
+    });
+
+    // 슬라이드 VEGAS
+    $(".main-background").vegas({
+        timer: false,
+        slides: [
+            { src: "images/slide-1.jpg" },
+            { src: "images/slide-2.jpg" },
+            { src: "images/slide-3.jpg" },
+            { src: "images/slide-4.jpg" }
+        ]
+    });
+    
 });
